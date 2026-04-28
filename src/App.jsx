8be0da85c8f3f37907b0216b649b72acba9d8af4
mt01366 <mt01366@surrey.ac.uk>
2026-04-28 10:36:10 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Banner from "./components/Banner.jsx";
+import "./App.css"
+function App() {
+    return (
+        <>
+            <Banner headerText="Providing houses all over the world"/>
+        </>
+    );
+}
+
+export default App
---
+++ src/components/Banner.jsx
@@ -0,0 +1,22 @@
+import logo from "../assets/GloboLogo.png";
+import {logo as logoClass} from "./Banner.module.css";
+
+const subtitleStyle = {
+    fontStyle: "italic",
+    fontSize: "x-large",
+    color: "coral",
+};
+
+const Banner = (props) => {
+  return (
+      <header className="row mb-4">
+          <div className="col-5">
+              <img src={logo} className={logoClass} alt="logo"/>
+          </div>
+          <div className="col-7 mt-5" style={subtitleStyle}>
+              {props.headerText}
+          </div>
+      </header>
+  );
+}
+export default Banner;
